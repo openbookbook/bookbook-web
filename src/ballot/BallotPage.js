458d78fbd8194a2b0b4f318aa54cc7e2fc4e0d04
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-24 15:06:24 -0700

    add username to fakeSuggestions and name to fakeBa

--- src/ballot/BallotPage.js
+++ src/ballot/BallotPage.js
@@ -4,36 +4,41 @@ import './BallotPage.css';
 const fakeBallot = {
   id: 1,
   adminCode: '1234',
+  name: 'bookclub',
+  voteCode: null
 };
 
 const fakeSuggestions = [
   {
     id: 1,
-    title: 'The Fish Warrior', 
+    username: 'bookperson123',
+    title: 'The Fish Warrior',
     ballotId: 1
   },
   {
     id: 2,
-    title: 'Crying in H-Mart', 
+    username: 'bookperson123',
+    title: 'Crying in H-Mart',
     ballotId: 1
   },
   {
     id: 3,
-    title: 'The Confederacy of Dunces', 
+    username: 'bookperson123',
+    title: 'The Confederacy of Dunces',
     ballotId: 1
   }
 ];
 
 const fakeVotes = [
-  
+
   {
-    userName:'daniella',
+    username: 'daniella',
     ballotId: 1,
     id: 123,
     vote: '3 1 2'
   },
   {
-    userName:'austin',
+    username: 'austin',
     ballotId: 1,
     id: 456,
     vote: '1 3 2'
@@ -49,7 +54,7 @@ export default class BallotPage extends Component {
     ballot: fakeBallot,
     suggestions: fakeSuggestions,
     votes: fakeVotes
-  } 
+  }
 
   onAdminInput = e => {
     e.preventDefault();
@@ -64,10 +69,10 @@ export default class BallotPage extends Component {
   render() {
     return (
       <div className="BallotPage">
-        <LoginPanel onAdminInput={this.onAdminInput}/>
-        <VotingPanel/>
+        <LoginPanel onAdminInput={this.onAdminInput} />
+        <VotingPanel />
         {/*only load adminpanel if showadmin is true, showadmin is true on if the code is entered*/}
-        {this.state.showAdmin && <AdminPanel/>}
+        {this.state.showAdmin && <AdminPanel />}
       </div>
     );
   }
@@ -75,19 +80,19 @@ export default class BallotPage extends Component {
 }
 
 class LoginPanel extends Component {
-  
+
   handleAdminInput = e => {
     this.props.onAdminInput(e);
   }
 
   render() {
-   
+
     return (
       <div className="LoginPanel">
         <form>
-          <input placeholder="name"/>
-          <input placeholder="password"/>
-          <input placeholder="admin code" onChange={this.handleAdminInput}/>
+          <input placeholder="name" />
+          <input placeholder="password" />
+          <input placeholder="admin code" onChange={this.handleAdminInput} />
         </form>
       </div>
     );
@@ -96,12 +101,12 @@ class LoginPanel extends Component {
 }
 
 class VotingPanel extends Component {
-  
+
   render() {
     return (
       <div className="VotingPanel">
         <p>This ballot uses ranked choice voting to vote.  Please put the books in the order that you most desire to read them.</p>
-        
+
       </div>
     );
   }
@@ -109,7 +114,7 @@ class VotingPanel extends Component {
 }
 
 class AdminPanel extends Component {
-  
+
   render() {
     return (
       <div className="AdminPanel">
