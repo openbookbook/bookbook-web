f2a2196dbb755d72e837c91c89b9f1a121b80caa
tif-calin <culitif@tuta.io>
2021-05-30 10:00:59 -0700

    add base62 encoder

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -37,3 +37,18 @@ export function shuffleArray(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
+export const base62 = {
+  charset: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+  encode: integer => {
+    if (integer === 0) return 0;
+
+    let s = [];
+    while (integer > 0) {
+      s = [base62.charset[integer % 62], ...s];
+      integer = Math.floor(integer / 62);
+    }
+    return s.join('');
+  },
+  decode: chars => chars.split('').reverse().reduce((prev, curr, i) => prev + (base62.charset.indexOf(curr) * (62 ** i)), 0)
+};
