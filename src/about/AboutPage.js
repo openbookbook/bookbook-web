0c4557631f3412e1148e9ee29d8ef19a034ed0b3
tif-calin <culitif@tuta.io>
2021-05-29 17:47:16 -0700

    format about page to use panels

--- src/about/AboutPage.js
+++ src/about/AboutPage.js
@@ -6,15 +6,20 @@ export default class AboutPage extends Component {
 
   render() {
     return (
-      <div className="AboutPage panel">
-        <p>bookbook helps create a more equitable process when choosing a new book for your club. bookbook allows a club admin to create a ballot where users can vote in a ranked choice voting system.</p>
-        <br></br>
-        <h3>what our creators were reading before starting alchemy :</h3>
-        <p>Annaleigh: 2001 Space Odyssey by Arthur C. Clarke</p>
-        <p>Austin: The Autistic Brain by Temple Grandin</p>
-        <p>Clem: Delirious New York by Rem Koolhaas</p>
-        <p>Culi: Arts of Living on a Damaged Planet</p>
-        <p>Daniella: No room for books only beans by B. Earl Nickels</p>
+      <div className="AboutPage page">
+        <span className="panel-title">description</span>
+        <div className="panel">
+          <p>bookbook helps create a more equitable process when choosing a new book for your club. bookbook allows a club admin to create a ballot where users can vote in a ranked choice voting system</p>
+        </div>
+
+        <span className="panel-title">creators</span>
+        <div className="panel">
+          <p>Annaleigh: 2001 Space Odyssey by Arthur C. Clarke</p>
+          <p>Austin: The Autistic Brain by Temple Grandin</p>
+          <p>Clem: Delirious New York by Rem Koolhaas</p>
+          <p>Culi: Arts of Living on a Damaged Planet</p>
+          <p>Daniella: No room for books only beans by B. Earl Nickels</p>
+        </div>
       </div>
     );
   }
