ad553bc5734471c06a46fa55f85b73e6060e15e7
culi <culitif@tuta.io>
2021-10-22 17:57:11 -0700

    use functional components

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -1,27 +1,22 @@
-import { Component } from 'react';
 import './AboutPage.css';
 
+const AboutPage = () => {
+  return <div className="AboutPage page">
+    <span className="panel-title">description</span>
+    <div className="panel">
+      <p>bookbook helps create a more equitable process when choosing a new book for your club</p>
+      <p>bookbook allows a club admin to create a ballot where users can vote in a ranked choice voting system</p>
+    </div>
 
-export default class AboutPage extends Component {
+    <span className="panel-title">creators</span>
+    <div className="panel">
+      <p>Annaleigh: 2001 Space Odyssey by Arthur C. Clarke</p>
+      <p>Austin: The Autistic Brain by Temple Grandin</p>
+      <p>Clem: Delirious New York by Rem Koolhaas</p>
+      <p>Culi: Arts of Living on a Damaged Planet</p>
+      <p>Daniella: No room for books only beans by B. Earl Nickels</p>
+    </div>
+  </div>;
+};
 
-  render() {
-    return (
-      <div className="AboutPage page">
-        <span className="panel-title">description</span>
-        <div className="panel">
-          <p>bookbook helps create a more equitable process when choosing a new book for your club. bookbook allows a club admin to create a ballot where users can vote in a ranked choice voting system</p>
-        </div>
-
-        <span className="panel-title">creators</span>
-        <div className="panel">
-          <p>Annaleigh: 2001 Space Odyssey by Arthur C. Clarke</p>
-          <p>Austin: The Autistic Brain by Temple Grandin</p>
-          <p>Clem: Delirious New York by Rem Koolhaas</p>
-          <p>Culi: Arts of Living on a Damaged Planet</p>
-          <p>Daniella: No room for books only beans by B. Earl Nickels</p>
-        </div>
-      </div>
-    );
-  }
-
-}
+export default AboutPage;
