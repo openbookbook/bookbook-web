a33b7d8fd5df3abf305f31bc843c208517680871
culi <culitif@tuta.io>
2021-10-23 01:04:53 -0700

    suggestpanel

---
+++ src/pages/setup/useGoogleBooks.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { searchBooks } from '../../utils/gbooks-api';
+
+const useGoogleBooks = () => {
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async query => {
+    try {
+      query = query.trim();
+
+      setResults(query ? await searchBooks(query) : []);
+
+    } catch (err) { console.error(err); }
+  };
+
+  return {
+    results,
+    handleSearch
+  };
+};
+
+export default useGoogleBooks;
