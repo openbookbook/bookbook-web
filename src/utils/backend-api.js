e5d8dbc30e6524a3161f1c288b13e5ec35a83bd0
culi <culitif@tuta.io>
2021-12-03 15:32:13 -0800

    clean up backend-api

--- src/utils/backend-api.js
+++ src/utils/backend-api.js
@@ -5,55 +5,37 @@ const URL = process.env.NODE_ENV === 'development'
   : 'https://openbookbook.herokuapp.com'
 ;
 
-const postReq = async (path, data) => {
-  const resp = await request
-    .post(`${URL}/${path}`)
-    .send(data)
+const req = async (method, path, data) => {
+  const resp = data 
+    ? await request[method](`${URL}/${path}`).send(data)
+    : await request[method](`${URL}/${path}`)
   ;
 
   if (resp.status >= 400) throw resp.body;
 
   return resp.body;
 };
+const postReq = async (path, data) => await req('post', path, data);
+const getReq = async (path) => await req('get', path);
+const putReq = async (path, data) => await req('put', path, data);
+const patchReq = async (path, data) => await req('patch', path, data);
+const deleteReq = async (path) => await req('delete', path);
 
-const getReq = async (path) => {
-  const resp = await request.get(`${URL}/${path}`);
-
-  if (resp.status >= 400) throw resp.body;
-
-  return resp.body;
-};
-
-const putReq = async (path, data) => {
-  const resp = await request
-    .put(`${URL}/${path}`)
-    .send(data)
-  ;
-
-  if (resp.status >= 400) throw resp.body;
-
-  return resp.body;
-};
-
-const deleteReq = async (path) => {
-  const resp = await request.delete(`${URL}/${path}`);
-
-  if (resp.status >= 400) throw resp.body;
-
-  return resp.body;
-};
-
+/* ballots */
 const addBallot = async (ballot) => postReq('api/ballots', ballot);
 const getBallot = async (ballotId) => getReq(`api/ballots/${ballotId}`);
 const updateBallot = async (ballot) => putReq(`api/ballots/${ballot.id}`, ballot);
 
+/* suggestions */
 const addSuggestion = async (suggestion) => postReq('api/suggestions', suggestion);
 const getSuggestions = async (ballotId) => getReq(`api/suggestions?ballot=${ballotId}`);
 const deleteSuggestion = async (id) => deleteReq(`api/suggestions/${id}`);
 
+/* users */
 const addUser = async (user) => postReq('api/users', user);
 const loginUser = async (user) => postReq(`api/users/${user.id}/login`, user);
 const updateUser = async (user) => putReq(`api/users/${user.id}`, user);
+const patchUser = async (user, patch) => patchReq(`api/users/${user.id}`, patch);
 const getUsers = async (ballotId) => getReq(`api/users?ballot=${ballotId}`);
 
 export {
@@ -66,5 +48,6 @@ export {
   addUser,
   loginUser,
   updateUser,
+  patchUser,
   getUsers
 };
