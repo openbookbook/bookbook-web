870e11f0aa940afa5f2b1d13fc075f3180a2130a
culi🌸 <culitif@tuta.io>
2022-05-07 14:33:20 -0700

    minor touch ups

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -6,12 +6,14 @@ const AboutPage = () => {
     <h3 className="page-title">about</h3>
     <span className="panel-title">description</span>
     <div className="panel">
-      <p>bookbook helps create a more equitable process when choosing a new book for your club</p>
+      <p><strong>bookbook</strong> helps create a more equitable process when choosing a new book for your club</p>
       <br/>
-      <p>bookbook allows a club admin to create a ballot where users can vote in a ranked choice voting system</p>
+      <p>
+        It allows a club to create a ballot where members can vote for their next book using their preferred voting method. We do not require a sign up or store any cookies or other data besides what is submitted.
+      </p>
     </div>
 
-    <span className="panel-title">creators</span>
+    <span className="panel-title">creators: what we're reading</span>
     <div className="panel">
       <p>Annaleigh: 2001 Space Odyssey by Arthur C. Clarke</p>
       <p>Austin: The Autistic Brain by Temple Grandin</p>
