85928371114f6c407695dabcad807985753fec0c
culi <culitif@tuta.io>
2024-10-12 19:59:34 -0700

    feat: Update styling and copy

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
-  return <footer className="Footer">	
-    &#127279; <a href="https://github.com/openbookbook/bookbook-web">steal this content</a>
-  </footer>;
-};
- 
+const Footer = () => (
+  <footer className="Footer">
+    {/* &#127279; */}
+    <a href="https://github.com/openbookbook/bookbook-web">&#9760; steal this content &#9760;</a>
+  </footer>
+);
+
 export default Footer;
--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import './AboutPage.css';
 
-const AboutPage = () => {
-  return <div className="AboutPage page">
+const AboutPage = () => (
+  <div className="AboutPage page">
     <h3 className="page-title">about</h3>
     <span className="panel-title">description</span>
     <div className="panel">
-      <p><strong>bookbook</strong> helps create a more equitable process when choosing a new book for your club</p>
+      <p>
+        <strong>bookbook</strong> is democratizes the process of selecting a new book for your book club.
+      </p>
       <br/>
       <p>
-        It allows a club to create a ballot where members can vote for their next book using their preferred voting method. We do not require a sign up or store any cookies or other data besides what is submitted.
+        It allows a club to create a ballot where members can vote for their next book using their preferred voting method.
+      </p>
+      <p>
+        We do <strong>not</strong> require a sign up or store any cookies or other data besides what is submitted. This system is inspired by services like <a href="https://www.when2meet.com/?27006387-2sgje">When2Meet</a> and our respect for your privacy.
       </p>
     </div>
 
     <span className="panel-title">creators: what we're reading</span>
     <div className="panel">
       <p>Annaleigh: 2001 Space Odyssey by Arthur C. Clarke</p>
+      <br />
       <p>Austin: The Autistic Brain by Temple Grandin</p>
+      <br />
       <p>Clem: Delirious New York by Rem Koolhaas</p>
+      <br />
       <p>Culi: Arts of Living on a Damaged Planet</p>
+      <br />
       <p>Daniella: No room for books only beans by B. Earl Nickels</p>
     </div>
-  </div>;
-};
-
-const MemoizedAboutPage = React.memo(AboutPage);
+  </div>
+);
 
 export default AboutPage;
